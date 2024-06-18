78aea0180c56ecf390a4efa7fa36a9c0d9472292
Sang <108661792+SangMunnKim@users.noreply.github.com>
2024-06-18 22:40:46 +0200

    Initial setup

---
+++ script.js
@@ -0,0 +1,26 @@
+function add(a,b){
+    return a+b;
+};
+
+function substract(a,b){
+    return a-b;
+};
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+let number1 = Number;
+let number2 = Number;
+let operator;
+
+function operate(number1, number2, operator){
+    if(operator == 'add'){
+        add(number1,number2);
+    }
+    else if(operate == '')
+}
