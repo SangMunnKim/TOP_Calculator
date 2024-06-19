a403cbdf1f8dd4498443069c1ad957c32c017420
Sang <108661792+SangMunnKim@users.noreply.github.com>
2024-06-19 14:52:47 +0200

    Got the displays and buttons to work

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
+const smallDisplay = document.querySelector('#smallDisplay');
+const display = document.querySelector('#display');
+const numberButton = [...document.querySelectorAll('.button.number')];
+const operatorButton = [...document.querySelectorAll('.button.operator:not(#sum)')];
+const sumButton = document.querySelector('#sum');
+const clearButton = document.querySelector('#clear');
+const deleteButton = document.querySelector('#delete');
+
+// Event listeners for number buttons
+numberButton.forEach(button => button.addEventListener('click', () => {
+    updateDisplay(button.textContent);
+}));
+
+numberButton.forEach(button => button.addEventListener('click', () => {
+    updateSmallDisplay(button.textContent);
+    scrollToEnd(smallDisplay);
+    }
+));
+
+operatorButton.forEach(button => button.addEventListener('click', () => {
+    const lastCharDisplay = display.textContent.slice(-1);
+    const lastCharSmallDisplay = smallDisplay.textContent.slice(-1);
+
+    if (!isOperator(lastCharDisplay)) {
+        updateDisplay(button.textContent);
+    }
+
+    if (!isOperator(lastCharSmallDisplay)) {
+        updateSmallDisplay(button.textContent);
+    }
+    }
+));
+
+clearButton.addEventListener('click', () => {
+    updateDisplay(''); 
+    updateSmallDisplayDelete('');
+});
+
+deleteButton.addEventListener('click', () => {
+    updateDisplay(display.textContent.slice(0,-1));
+    updateSmallDisplayDelete(smallDisplay.textContent.slice(0,-1))
+});
+
+function updateDisplay(value){
+    display.textContent = value;
+}
+
+function updateSmallDisplay(value){
+    if(smallDisplay.textContent == ''){
+        smallDisplay.textContent = value;
+    } else{
+        smallDisplay.textContent += value;
+    }
+};
+
+function updateSmallDisplayDelete(value){
+    smallDisplay.textContent = value;
+}
+
+
+function scrollToEnd(element) {
+    element.scrollLeft = element.scrollWidth;
+}
+
+function isOperator(character) {
+    return ['+', '−', '×', '/'].includes(character);
+}
+
 function add(a,b){
     return a+b;
 };
@@ -14,13 +82,20 @@ function divide(a,b){
     return a/b;
 }
 
-let number1 = Number;
-let number2 = Number;
-let operator;
-
 function operate(number1, number2, operator){
     if(operator == 'add'){
         add(number1,number2);
     }
-    else if(operate == '')
+    else if(operate == 'substract'){
+        substract(number1, number2);
+    }
+    else if(operate == 'multiply'){
+        multiply(number1,number2);
+    }
+    else if(operate == 'divide'){
+        divide(number1,number2);
+    }
 }
+
+
+
